ceebd42ffd182ab099722af84a9a5e9ba9717d53
ChengTao <ctparadisehell@gmail.com>
2018-05-04 09:21:34 +0800

    feat: 开启跨域

--- backend/src/main/kotlin/com/chengtao/bikesharing/Application.kt
+++ backend/src/main/kotlin/com/chengtao/bikesharing/Application.kt
@@ -2,9 +2,21 @@ package com.chengtao.bikesharing
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.context.annotation.Bean
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @SpringBootApplication
-class Application
+class Application {
+  @Bean
+  fun corsConfigurer(): WebMvcConfigurer {
+    return object : WebMvcConfigurer {
+      override fun addCorsMappings(registry: CorsRegistry?) {
+        registry?.addMapping("/*")?.allowedOrigins("*") ?: super.addCorsMappings(registry)
+      }
+    }
+  }
+}
 
 fun main(args: Array<String>) {
   SpringApplication.run(Application::class.java, *args)
