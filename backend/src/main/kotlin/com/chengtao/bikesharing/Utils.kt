3a21ce390b2742e7b251c18eaabc5fc419ea77a8
ChengTao <ctparadisehell@gmail.com>
2018-05-04 08:17:57 +0800

    refactor: 时间格式支持 yyyy-MM-dd'T'HH:mm:ssZ 和 yyyy-MM-dd'T'Z

--- backend/src/main/kotlin/com/chengtao/bikesharing/Utils.kt
+++ backend/src/main/kotlin/com/chengtao/bikesharing/Utils.kt
@@ -3,23 +3,27 @@ package com.chengtao.bikesharing
 import java.text.ParseException
 import java.text.SimpleDateFormat
 import java.util.Date
-import java.util.TimeZone
 
 object Utils {
   /**
    * 字符串转换成时间
-   * 格式 yyyy-MM-dd
+   * 格式 yyyy-MM-dd'T'HH:mm:ssZ | yyyy-MM-dd'T'Z
    */
   fun rfc3339ToDate(date: String?): Date? {
     return if (date == null || date == "") {
       null
     } else
       try {
-        val dateFormat = SimpleDateFormat("yyy-MM-dd'T'Z")
+        val dateFormat = SimpleDateFormat("yyy-MM-dd'T'HH:mm:ssZ")
         dateFormat.parse(date)
       } catch (e: ParseException) {
-        println("e : ${e.message}")
-        null
+        try {
+          val dateFormat = SimpleDateFormat("yyy-MM-dd'T'Z")
+          dateFormat.parse(date)
+        } catch (e: ParseException) {
+          println("e : ${e.message}")
+          null
+        }
       }
   }
 }
