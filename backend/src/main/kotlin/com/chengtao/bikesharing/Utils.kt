11bb5b7598139d82e760d33a9cfb462d54fff120
ChengTao <ctparadisehell@gmail.com>
2018-05-05 20:10:56 +0800

    refactor: 规范时间格式,使用 RFC3339 ("yyyy-MM-dd'T'HH:mm:ssZ")

--- backend/src/main/kotlin/com/chengtao/bikesharing/Utils.kt
+++ backend/src/main/kotlin/com/chengtao/bikesharing/Utils.kt
@@ -3,21 +3,18 @@ package com.chengtao.bikesharing
 import java.text.ParseException
 import java.text.SimpleDateFormat
 import java.util.Date
-import java.util.TimeZone
 
 object Utils {
   /**
    * 字符串转换成时间
-   * 格式 yyyy-MM-dd'T'HH:mm:ss'Z'(UTC)
+   * 格式 yyyy-MM-dd'T'HH:mm:ssZ
    */
   fun rfc3339ToDate(date: String?): Date? {
     return if (date == null || date == "") {
       null
     } else
       try {
-        val dateFormat = SimpleDateFormat("yyy-MM-dd'T'HH:mm:ss'Z'")
-        dateFormat.timeZone = TimeZone.getTimeZone("UTC")
-        println("timeZone : " + dateFormat.timeZone.displayName)
+        val dateFormat = SimpleDateFormat("yyy-MM-dd'T'HH:mm:ssZ")
         dateFormat.parse(date)
       } catch (e: ParseException) {
         null
--- backend/src/test/kotlin/Test.kt
+++ backend/src/test/kotlin/Test.kt
@@ -1,15 +1,15 @@
 import java.text.SimpleDateFormat
-import java.util.Date
-import java.util.TimeZone
+import java.util.Calendar
 
 fun main(args: Array<String>) {
   dateTest()
 }
 
 fun dateTest() {
-  val date = Date()
-  val dateFormat = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'");
-  dateFormat.timeZone = TimeZone.getTimeZone("UTC")
-  val dateString = dateFormat.format(date);
-  println("date : $dateString")
+  val dateString = "2016-08-01T00:00:00+0800";
+  val dateFormat = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssZ");
+  val date = dateFormat.parse(dateString)
+  val calendar = Calendar.getInstance()
+  calendar.time = date
+  println("date : $dateString time-zone : ${calendar.timeZone}")
 }
