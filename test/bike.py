caa19ed584c3d30985390da20b6d01105d54ac5b
ChengTao <ctparadisehell@gmail.com>
2018-05-06 22:31:28 +0800

    refactor: 规范 .gitignore

--- test/bike.py
+++ test/bike.py
@@ -6,11 +6,11 @@ import requests
 createBikeAPI="http://localhost:9999/bikes"
 
 #ofo小黄车
-ofoPlayload = {'name':'ofo小黄车','founded_at':'2015-03-25T16:00:00Z'}
+ofoPlayload = {'name':'ofo小黄车','founded_at':'2015-03-26T00:00:00+0800'}
 ofoCreateReequest = requests.post(createBikeAPI,ofoPlayload);
 print(ofoCreateReequest.json())
 
 #摩拜单车
-mobikePlayload = {'name':'摩拜单车','founded_at':'2015-01-26T16:00:00Z'}
+mobikePlayload = {'name':'摩拜单车','founded_at':'2015-01-27T00:00:00+0800'}
 mobikeCreateReequest = requests.post(createBikeAPI,mobikePlayload);
 print(mobikeCreateReequest.json())
