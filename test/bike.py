6fc69c8b6308b2f8ef415703d209fddadae0f792
ChengTao <ctparadisehell@gmail.com>
2018-05-10 19:14:25 +0800

    feat: 添加测试发展数据

--- test/bike.py
+++ test/bike.py
@@ -3,7 +3,7 @@
 import requests
 
 #api
-createBikeAPI="http://localhost:9999/bikes"
+createBikeAPI="http://localhost:9999/bikes";
 
 #ofo小黄车
 ofoPlayload = {
@@ -12,7 +12,7 @@ ofoPlayload = {
         'introduction': 'ofo 小黄车是全球第一个无桩共享单车出行平台，首创“无桩单车共享”模式。用户只需在ofo官方App、支付宝小程序、微信服务号和微信小程序输入车牌号或扫码，即可解锁用车，随取随用，随时随地，也可以共享自己的单车到 ofo 共享平台，获得所有 ofo 小黄车的终身免费使用权，以1换N。'
 }
 ofoCreateReequest = requests.post(createBikeAPI,ofoPlayload);
-print(ofoCreateReequest.json())
+print(ofoCreateReequest.json());
 
 #摩拜单车
 mobikePlayload = {
@@ -21,4 +21,4 @@ mobikePlayload = {
         'introduction': '摩拜单车创建了全球首个智能共享单车模式，自主研发的专利智能锁集成了 GPS 和通讯模块，使用了新一代物联网技术，通过智能手机 app 让用户随时随地可以定位并使用最近的摩拜单车，骑行到达目的地后，就近停放在路边合适的区域，关锁即实现电子付费结算。'
 }
 mobikeCreateReequest = requests.post(createBikeAPI,mobikePlayload);
-print(mobikeCreateReequest.json())
+print(mobikeCreateReequest.json());
---
+++ test/development.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*- 
+
+import requests
+import json
+
+#api
+getBikeListAPI="http://localhost:9999/bikes";
+createDevlopmentAPI="http://localhost:9999/developments";
+
+#获取所有单车
+getBikeListRequest = requests.get(getBikeListAPI);
+success = 0;
+bikeDictionary = {};
+if getBikeListRequest.status_code == 200:
+    success = 1; 
+    json = getBikeListRequest.json();
+    for i in range(len(json)):
+        bike = json[i];
+        bikeDictionary[bike['name']] = bike['id'];
+else:
+    success = 0;
+    print(getBikeListRequest.json());
+
+if success == 1:
+    print(bikeDictionary);
+    #读取文件
+    try:
+        developments = open("./development.txt","r");
+        developmentList = [];
+        try:
+            for development in developments:
+                development = development.strip();
+                if development != "":
+                    developmentList.append(development); 
+        finally:
+            developments.close();
+        developmentPlaylod = {};
+        for i in range(len(developmentList)):
+            detailList = developmentList[i].split(",");
+            if len(detailList) == 4:
+                developmentPlaylod.clear();
+                #
+                bikeName = detailList[0];
+                if bikeName in bikeDictionary:
+                    bikeId = bikeDictionary.get(bikeName);
+                    city = detailList[1];
+                    deliveryAt = detailList[2];
+                    deliveryCount = detailList[3];
+                    #
+                    developmentPlaylod['bike_id'] = bikeId;
+                    developmentPlaylod['city'] = city;
+                    developmentPlaylod['delivery_at'] = deliveryAt;
+                    developmentPlaylod['delivery_count'] = deliveryCount;
+                    #
+                    createDevlopmentRequest = requests.post(createDevlopmentAPI,developmentPlaylod);
+    except IOError:
+        print("open file failed");
